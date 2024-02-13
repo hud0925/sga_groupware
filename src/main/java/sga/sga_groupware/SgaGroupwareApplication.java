60ac1a39089223056f2842066621add3c65d1614
hwanggun <udhwang@sgacorp.kr>
2024-02-13 11:27:22 +0900

    aa

---
+++ src/main/java/sga/sga_groupware/SgaGroupwareApplication.java
@@ -0,0 +1,13 @@
+package sga.sga_groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaGroupwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgaGroupwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sga/sga_groupware/SgaGroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package sga.sga_groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaGroupwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
